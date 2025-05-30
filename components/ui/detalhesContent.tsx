83c9a8d43a2090cdf7027394f815ec66b914eb11
marcospeneda <marcospaulodp16@gmail.com>
2025-05-30 19:54:09 -0300

    vapo

---
+++ components/ui/detalhesContent.tsx
@@ -0,0 +1,9 @@
+//importa os componentes basicos da interface do React Native//
+import {View, Text, StyleSheet, FlatList} from "react-native";
+//lista de dados ficticios a serem exibidos no componente//
+const dados = [
+    { id:'1', descrição 'transferencia recebida -R$120,00'},
+    { id:'2', descrição 'pagamento de boleto -R$60,00'},
+    { id:'3', descrição 'PIX enviado R$200,00'},
+    { id:'4', descrição 'deposito R$500,00'}
+];
